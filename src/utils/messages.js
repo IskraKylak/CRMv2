2891c7aac7a11821b68bed57632b5613c04bda9b
IskraKylak <kmlider001@gmail.com>
2021-11-29 11:08:01 +0200

    test2

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,7 +1,6 @@
 export default {
   'logout': 'Вы вышли из системы',
   'auth/user-not-found': 'Такого дурочка в базе данных нет!',
-  'auth/wrong-password': 'Кто то забрал твой пароль твой пароль!',
-  'auth/wrong-password1': 'Кто то забрал твой пароль твой пароль!'
+  'auth/wrong-password': 'Кто то украл твой пароль твой пароль!'
 }
 
